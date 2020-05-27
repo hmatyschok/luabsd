70a80e684fc8de584fe7093855174fe62cff21ce
tsag <henning.matyschok@outlook.com>
2020-05-28 01:53:21 +0200

    Typo fixed.

--- bsd.lua
+++ bsd.lua
@@ -1,11 +1,6 @@
 
 local path = "/usr/lib/libluabsd.so"
 local func = "luaopen_bsd"
-local f = assert(loadlib(path, func))
-local function ret(s, ...)
-    if s then
-        return unpack(arg)
-    end
-    error(path .. ": " .. func .. " failed.")
-end
-return ret(pcall(f))
+local f = assert(package.loadlib(path, func))
+return f()
+
