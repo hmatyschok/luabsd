94af5c8de8cb6ed015f59878f854af37d5a0e2ab
tsag <henning.matyschok@outlook.com>
2020-11-02 11:23:20 +0100

    Service primitives over <pwd.h>.
         struct passwd *
         getpwent(void);
    
         int
         getpwent_r(struct passwd *pwd, char *buffer, size_t bufsize,
             struct passwd **result);
    
         struct passwd *
         getpwnam(const char *login);
    
         int
         getpwnam_r(const char *name, struct passwd *pwd, char *buffer,
             size_t bufsize, struct passwd **result);
    
         struct passwd *
         getpwuid(uid_t uid);
    
         int
         getpwuid_r(uid_t uid, struct passwd *pwd, char *buffer, size_t bufsize,
             struct passwd **result);
    
         int
         setpassent(int stayopen);
    
         void
         setpwent(void);
    
         void
         endpwent(void);
    
     Synopsis
    
        #1: passwd [, err , msg ] = bsd.pwd.getpwent()
    
        #2: pwd, result = (LUA_TUSERDATA(PASSWD)), (LUA_TUSERDATA(PASSWD))
            buffer, buflen = (LUA_TUSERDATA(IOVEC)), (LUA_TNUMBER)
    
            ret [, err, msg ] = bsd.pwd.getpwent_r(pwd, buffer, buflen, result)
    
        #3: stayopen = (LUA_TNUMBER)
    
            ret [, err, msg ] = bsd.pwd.setpassent(stayopen)
    
        #4: login = (LUA_TSTRING)
    
            passwd [, err, msg ] = bsd.pwd.getpwnam(login)
    
        #5: name = (LUA_TSTRING)
    
            ret [, err, msg ] = bsd.pwd.getpwnam_r(name, pwd, buffer, bufsize, result)
    
        #6: uid = (LUAB_TNUMBER)
    
            passwd [, err, msg ] = bsd.pwd.getpwuid(uid)
    
        #7: ret [, err, msg ] = bsd.pwd.getpwuid_r(uid, pwd, buffer, bufsize, result)
    
        #8: ret [, err, msg ] = bsd.pwd.setpwent()
    
        #9: ret [, err, msg ] = bsd.pwd.endpwent()

--- src/modules/luab_pwd.c
+++ src/modules/luab_pwd.c
@@ -42,6 +42,356 @@ extern luab_module_t luab_pwd_lib;
  * Service primitves.
  */
 
+/***
+ * getpwnam(3) - password database operations
+ *
+ * @function getpwnam
+ *
+ * @param login             Specifies user name.
+ *
+ * @return (LUA_T{NIL,USERDATA} [, LUA_T{NIL,NUMBER}, LUA_T{NIL,STRING} ])
+ *
+ * @usage passwd [, err, msg ] = bsd.pwd.getpwnam(login)
+ */
+static int
+luab_getpwnam(lua_State *L)
+{
+    const char *login;
+    struct passwd *pwd;
+    int status;
+
+    (void)luab_core_checkmaxargs(L, 1);
+
+    login = luab_checklstring(L, 1, MAXLOGNAME);
+
+    if ((pwd = getpwnam(login)) != NULL)
+        status = luab_pushudata(L, luab_mx(PASSWD), pwd);
+    else
+        status = luab_pushnil(L);
+
+    return (status);
+}
+
+/***
+ * getpwuid(3) - password database operations
+ *
+ * @function getpwuid
+ *
+ * @param uid               Specifies user ID.
+ *
+ * @return (LUA_T{NIL,USERDATA} [, LUA_T{NIL,NUMBER}, LUA_T{NIL,STRING} ])
+ *
+ * @usage passwd [, err, msg ] = bsd.pwd.getpwuid(uid)
+ */
+static int
+luab_getpwuid(lua_State *L)
+{
+    uid_t uid;
+    struct passwd *pwd;
+    int status;
+
+    (void)luab_core_checkmaxargs(L, 1);
+
+    uid = luab_checkinteger(L, 1, INT_MAX);
+
+    if ((pwd = getpwuid(uid)) != NULL)
+        status = luab_pushudata(L, luab_mx(PASSWD), pwd);
+    else
+        status = luab_pushnil(L);
+
+    return (status);
+}
+
+#if __XSI_VISIBLE >= 500
+/***
+ * endpwent(3) - password database operations
+ *
+ * @function endpwent
+ *
+ * @return (LUA_TNUMBER [, LUA_T{NIL,NUMBER}, LUA_T{NIL,STRING} ])
+ *
+ * @usage ret [, err, msg ] = bsd.pwd.endpwent()
+ */
+static int
+luab_endpwent(lua_State *L)
+{
+    (void)luab_core_checkmaxargs(L, 0);
+
+    endpwent();
+    return (luab_pusherr(L, 0));
+}
+
+/***
+ * getpwent(3) - password database operations
+ *
+ * @function getpwent
+ *
+ * @return (LUA_T{NIL,USERDATA} [, LUA_T{NIL,NUMBER}, LUA_T{NIL,STRING} ])
+ *
+ * @usage passwd [, err, msg ] = bsd.pwd.getpwent()
+ */
+static int
+luab_getpwent(lua_State *L)
+{
+    struct passwd *pwd;
+    int status;
+
+    (void)luab_core_checkmaxargs(L, 0);
+
+    if ((pwd = getpwent()) != NULL)
+        status = luab_pushudata(L, luab_mx(PASSWD), pwd);
+    else
+        status = luab_pushnil(L);
+
+    return (status);
+}
+
+/***
+ * setpwent(3) - password database operations
+ *
+ * @function setpwent
+ *
+ * @return (LUA_TNUMBER [, LUA_T{NIL,NUMBER}, LUA_T{NIL,STRING} ])
+ *
+ * @usage ret [, err, msg ] = bsd.pwd.setpwent()
+ */
+static int
+luab_setpwent(lua_State *L)
+{
+    (void)luab_core_checkmaxargs(L, 0);
+
+    setpwent();
+    return (luab_pusherr(L, 0));
+}
+#endif /* __XSI_VISIBLE */
+
+#if __POSIX_VISIBLE >= 200112 || __XSI_VISIBLE >= 500
+/***
+ * getpwnam_r(3) - password database operations
+ *
+ * @function getpwnam_r
+ *
+ * @param name              Specifies user name.
+ * @param pwd               Storage, instance of (LUA_TUSERDATA(PASSWD)).
+ * @param buffer            Line buffer, instance of (LUA_TUSERDATA(IOVEC)),
+ *                          see passwd(5) for further details.
+ * @param bufsize           By _SC_GETPW_R_SIZE_MAX over sysconf(3) determined
+ *                          size in bytes to hold at least one line by passwd(5)
+ *                          specified format.
+ * @param result            Result argument, instance of (LUA_TUSERDATA(PASSWD)).
+ *
+ * @return (LUA_TNUMBER [, LUA_T{NIL,NUMBER}, LUA_T{NIL,STRING} ])
+ *
+ * @usage ret [, err, msg ] = bsd.pwd.getpwnam_r(name, pwd, buffer, bufsize, result)
+ */
+static int
+luab_getpwnam_r(lua_State *L)
+{
+    const char *name;
+    struct passwd *pwd;
+    luab_iovec_t *buf;
+    size_t bufsize;
+    struct passwd *ret;
+    caddr_t bp;
+    struct passwd *result;
+    int status;
+
+    (void)luab_core_checkmaxargs(L, 5);
+
+    name = luab_checklstring(L, 1, MAXLOGNAME);
+    pwd = luab_udata(L, 2, luab_mx(PASSWD), struct passwd *);
+    buf = luab_udata(L, 3, luab_mx(IOVEC), luab_iovec_t *);
+    bufsize = (size_t)luab_checkinteger(L, 4,
+#ifdef  __LP64__
+    LONG_MAX
+#else
+    INT_MAX
+#endif
+    );
+    ret = luab_udata(L, 5, luab_mx(PASSWD), struct passwd *);
+
+    if (((bp = buf->iov.iov_base) != NULL) &&
+        (buf->iov_max_len <= LUAL_BUFFERSIZE) &&
+        (bufsize <= buf->iov_max_len) &&
+        (buf->iov_flags & IOV_BUFF)) {
+
+        if ((buf->iov_flags & IOV_LOCK) == 0) {
+            buf->iov_flags |= IOV_LOCK;
+
+            if ((status = getpwnam_r(name, pwd, bp, bufsize, &result)) != 0)
+                (void)memmove(ret, result, sizeof(struct passwd));
+
+            buf->iov_flags &= ~IOV_LOCK;
+        } else {
+            errno = EBUSY;
+            status = 0;
+        }
+    } else {
+        errno = ERANGE;
+        status = 0;
+    }
+    return (luab_pusherr(L, status));
+}
+
+/***
+ * getpwuid_r(3) - password database operations
+ *
+ * @function getpwuid_r
+ *
+ * @param uid               Specifies user ID.
+ * @param pwd               Storage, instance of (LUA_TUSERDATA(PASSWD)).
+ * @param buffer            Line buffer, instance of (LUA_TUSERDATA(IOVEC)),
+ *                          see passwd(5) for further details.
+ * @param bufsize           By _SC_GETPW_R_SIZE_MAX over sysconf(3) determined
+ *                          size in bytes to hold at least one line by passwd(5)
+ *                          specified format.
+ * @param result            Result argument, instance of (LUA_TUSERDATA(PASSWD)).
+ *
+ * @return (LUA_TNUMBER [, LUA_T{NIL,NUMBER}, LUA_T{NIL,STRING} ])
+ *
+ * @usage ret [, err, msg ] = bsd.pwd.getpwuid_r(uid, pwd, buffer, bufsize, result)
+ */
+static int
+luab_getpwuid_r(lua_State *L)
+{
+    uid_t uid;
+    struct passwd *pwd;
+    luab_iovec_t *buf;
+    size_t bufsize;
+    struct passwd *ret;
+    caddr_t bp;
+    struct passwd *result;
+    int status;
+
+    (void)luab_core_checkmaxargs(L, 5);
+
+    uid = luab_checkinteger(L, 1, INT_MAX);
+    pwd = luab_udata(L, 2, luab_mx(PASSWD), struct passwd *);
+    buf = luab_udata(L, 3, luab_mx(IOVEC), luab_iovec_t *);
+    bufsize = (size_t)luab_checkinteger(L, 4,
+#ifdef  __LP64__
+    LONG_MAX
+#else
+    INT_MAX
+#endif
+    );
+    ret = luab_udata(L, 5, luab_mx(PASSWD), struct passwd *);
+
+    if (((bp = buf->iov.iov_base) != NULL) &&
+        (buf->iov_max_len <= LUAL_BUFFERSIZE) &&
+        (bufsize <= buf->iov_max_len) &&
+        (buf->iov_flags & IOV_BUFF)) {
+
+        if ((buf->iov_flags & IOV_LOCK) == 0) {
+            buf->iov_flags |= IOV_LOCK;
+
+            if ((status = getpwuid_r(uid, pwd, bp, bufsize, &result)) != 0)
+                (void)memmove(ret, result, sizeof(struct passwd));
+
+            buf->iov_flags &= ~IOV_LOCK;
+        } else {
+            errno = EBUSY;
+            status = 0;
+        }
+    } else {
+        errno = ERANGE;
+        status = 0;
+    }
+    return (luab_pusherr(L, status));
+}
+#endif /* __POSIX_VISIBLE >= 200112 || __XSI_VISIBLE >= 500 */
+
+#if __BSD_VISIBLE
+/***
+ * setpassent(3) - password database operations
+ *
+ * @function setpassent
+ *
+ * @param stayopen          Specifies, if file descriptors from db(3) stays
+ *                          open when its value is set non-zero.
+ *
+ * @return (LUA_TNUMBER [, LUA_T{NIL,NUMBER}, LUA_T{NIL,STRING} ])
+ *
+ * @usage ret [, err, msg ] = bsd.pwd.setpassent(stayopen)
+ */
+static int
+luab_setpassent(lua_State *L)
+{
+    int stayopen;
+    int status;
+
+    (void)luab_core_checkmaxargs(L, 1);
+
+    stayopen = luab_checkinteger(L, 1, INT_MAX);
+    status = setpassent(stayopen);
+    return (luab_pusherr(L, status));
+}
+
+/***
+ * getpwent_r(3) - password database operations
+ *
+ * @function getpwent_r
+ *
+ * @param pwd               Storage, instance of (LUA_TUSERDATA(PASSWD)).
+ * @param buffer            Line buffer, instance of (LUA_TUSERDATA(IOVEC)),
+ *                          see passwd(5) for further details.
+ * @param bufsize           By _SC_GETPW_R_SIZE_MAX over sysconf(3) determined
+ *                          size in bytes to hold at least one line by passwd(5)
+ *                          specified format.
+ * @param result            Result argument, instance of (LUA_TUSERDATA(PASSWD)).
+ *
+ * @return (LUA_TNUMBER [, LUA_T{NIL,NUMBER}, LUA_T{NIL,STRING} ])
+ *
+ * @usage ret [, err, msg ] = bsd.pwd.getpwent_r(pwd, buffer, bufsize, result)
+ */
+static int
+luab_getpwent_r(lua_State *L)
+{
+    struct passwd *pwd;
+    luab_iovec_t *buf;
+    size_t bufsize;
+    struct passwd *ret;
+    caddr_t bp;
+    struct passwd *result;
+    int status;
+
+    (void)luab_core_checkmaxargs(L, 4);
+
+    pwd = luab_udata(L, 1, luab_mx(PASSWD), struct passwd *);
+    buf = luab_udata(L, 2, luab_mx(IOVEC), luab_iovec_t *);
+    bufsize = (size_t)luab_checkinteger(L, 3,
+#ifdef  __LP64__
+    LONG_MAX
+#else
+    INT_MAX
+#endif
+    );
+    ret = luab_udata(L, 4, luab_mx(PASSWD), struct passwd *);
+
+    if (((bp = buf->iov.iov_base) != NULL) &&
+        (buf->iov_max_len <= LUAL_BUFFERSIZE) &&
+        (bufsize <= buf->iov_max_len) &&
+        (buf->iov_flags & IOV_BUFF)) {
+
+        if ((buf->iov_flags & IOV_LOCK) == 0) {
+            buf->iov_flags |= IOV_LOCK;
+
+            if ((status = getpwent_r(pwd, bp, bufsize, &result)) != 0)
+                (void)memmove(ret, result, sizeof(struct passwd));
+
+            buf->iov_flags &= ~IOV_LOCK;
+        } else {
+            errno = EBUSY;
+            status = 0;
+        }
+    } else {
+        errno = ERANGE;
+        status = 0;
+    }
+    return (luab_pusherr(L, status));
+}
+#endif /* __BSD_VISIBLE */
+
 /*
  * Generator functions.
  */
@@ -102,6 +452,21 @@ static luab_module_table_t luab_pwd_vec[] = { /* pwd.h */
     LUAB_INT("_PWF_FILES",              _PWF_FILES),
     LUAB_INT("_PWF_NIS",                _PWF_NIS),
     LUAB_INT("_PWF_HESIOD",             _PWF_HESIOD),
+    LUAB_FUNC("getpwnam",               luab_getpwnam),
+    LUAB_FUNC("getpwuid",               luab_getpwuid),
+#if __XSI_VISIBLE >= 500
+    LUAB_FUNC("endpwent",               luab_endpwent),
+    LUAB_FUNC("getpwent",               luab_getpwent),
+    LUAB_FUNC("setpwent",               luab_setpwent),
+#endif
+#if __POSIX_VISIBLE >= 200112 || __XSI_VISIBLE >= 500
+    LUAB_FUNC("getpwnam_r",             luab_getpwnam_r),
+    LUAB_FUNC("getpwuid_r",             luab_getpwuid_r),
+#endif
+#ifdef __BSD_VISIBLE
+    LUAB_FUNC("setpassent",             luab_setpassent),
+    LUAB_FUNC("getpwent_r",             luab_getpwent_r),
+#endif
     LUAB_FUNC("passwd_create",          luab_passwd_create),
     LUAB_MOD_TBL_SENTINEL
 };
