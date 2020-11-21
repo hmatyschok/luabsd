e3db9ec7333dc7df10c2cb0741f8b23e62431b6c
tsag <henning.matyschok@outlook.com>
2020-11-21 02:27:24 +0100

    Extension, luab_fhandle(3).

--- src/composite/luab_fhandle_type.c
+++ src/composite/luab_fhandle_type.c
@@ -215,6 +215,67 @@ fhandle_udata(lua_State *L, int narg)
     return (luab_to_fhandle(L, narg));
 }
 
+static luab_table_t *
+fhandle_checktable(lua_State *L, int narg)
+{
+    luab_table_t *tbl;
+    fhandle_t *x, *y;
+    size_t m, n, sz;
+
+    sz = sizeof(fhandle_t);
+
+    if ((tbl = luab_newvectornil(L, narg, sz)) != NULL) {
+
+        if (((x = (fhandle_t *)tbl->tbl_vec) != NULL) &&
+            (tbl->tbl_card > 1)) {
+            luab_table_init(L, 0);
+
+            for (m = 0, n = (tbl->tbl_card - 1); m < n; m++) {
+
+                if (lua_next(L, narg) != 0) {
+
+                    if ((lua_isnumber(L, -2) != 0) &&
+                        (lua_isuserdata(L, -1) != 0)) {
+                        y = luab_udata(L, -1, &luab_fhandle_type, fhandle_t *);
+                        (void)memmove(&(x[m]), y, sz);
+                    } else
+                        luab_core_err(EX_DATAERR, __func__, EINVAL);
+                } else {
+                    errno = ENOENT;
+                    break;
+                }
+                lua_pop(L, 1);
+            }
+        }
+    }
+    return (tbl);
+}
+
+static void
+fhandle_pushtable(lua_State *L, int narg, luab_table_t *tbl, int new, int clr)
+{
+    fhandle_t *x;
+    size_t m, n, k;
+
+    if (tbl != NULL) {
+
+        if (((x = (fhandle_t *)tbl->tbl_vec) != NULL) &&
+            ((n = (tbl->tbl_card - 1)) != 0)) {
+            luab_table_init(L, new);
+
+            for (m = 0, k = 1; m < n; m++, k++)
+                luab_rawsetudata(L, narg, &luab_fhandle_type, k, &(x[m]));
+
+            errno = ENOENT;
+        } else
+            errno = ERANGE;
+
+        if (clr != 0)
+            luab_table_free(tbl);
+    } else
+        errno = EINVAL;
+}
+
 luab_module_t luab_fhandle_type = {
     .m_cookie   = LUAB_FHANDLE_TYPE_ID,
     .m_name     = LUAB_FHANDLE_TYPE,
@@ -222,5 +283,7 @@ luab_module_t luab_fhandle_type = {
     .m_create   = fhandle_create,
     .m_init     = fhandle_init,
     .m_get      = fhandle_udata,
+    .m_get_tbl  = fhandle_checktable,
+    .m_set_tbl  = fhandle_pushtable,
     .m_sz       = sizeof(luab_fhandle_t),
 };
