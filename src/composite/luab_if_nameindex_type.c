a6571bb845947cca6a971aef85f2fa52e6536609
tsag <henning.matyschok@outlook.com>
2020-11-21 02:42:26 +0100

    Extension, luab_if_nameindex(3).

--- src/composite/luab_if_nameindex_type.c
+++ src/composite/luab_if_nameindex_type.c
@@ -32,6 +32,7 @@
 
 #include "luabsd.h"
 #include "luab_udata.h"
+#include "luab_table.h"
 
 extern luab_module_t luab_if_nameindex_type;
 
@@ -223,6 +224,67 @@ if_nameindex_udata(lua_State *L, int narg)
     return (luab_to_if_nameindex(L, narg));
 }
 
+static luab_table_t *
+if_nameindex_checktable(lua_State *L, int narg)
+{
+    luab_table_t *tbl;
+    struct if_nameindex *x, *y;
+    size_t m, n, sz;
+
+    sz = sizeof(struct if_nameindex);
+
+    if ((tbl = luab_newvectornil(L, narg, sz)) != NULL) {
+
+        if (((x = (struct if_nameindex *)tbl->tbl_vec) != NULL) &&
+            (tbl->tbl_card > 1)) {
+            luab_table_init(L, 0);
+
+            for (m = 0, n = (tbl->tbl_card - 1); m < n; m++) {
+
+                if (lua_next(L, narg) != 0) {
+
+                    if ((lua_isnumber(L, -2) != 0) &&
+                        (lua_isuserdata(L, -1) != 0)) {
+                        y = luab_udata(L, -1, &luab_if_nameindex_type, struct if_nameindex *);
+                        (void)memmove(&(x[m]), y, sz);
+                    } else
+                        luab_core_err(EX_DATAERR, __func__, EINVAL);
+                } else {
+                    errno = ENOENT;
+                    break;
+                }
+                lua_pop(L, 1);
+            }
+        }
+    }
+    return (tbl);
+}
+
+static void
+if_nameindex_pushtable(lua_State *L, int narg, luab_table_t *tbl, int new, int clr)
+{
+    struct if_nameindex *x;
+    size_t m, n, k;
+
+    if (tbl != NULL) {
+
+        if (((x = (struct if_nameindex *)tbl->tbl_vec) != NULL) &&
+            ((n = (tbl->tbl_card - 1)) != 0)) {
+            luab_table_init(L, new);
+
+            for (m = 0, k = 1; m < n; m++, k++)
+                luab_rawsetudata(L, narg, &luab_if_nameindex_type, k, &(x[m]));
+
+            errno = ENOENT;
+        } else
+            errno = ERANGE;
+
+        if (clr != 0)
+            luab_table_free(tbl);
+    } else
+        errno = EINVAL;
+}
+
 luab_module_t luab_if_nameindex_type = {
     .m_cookie   = LUAB_IF_NAMEINDEX_TYPE_ID,
     .m_name     = LUAB_IF_NAMEINDEX_TYPE,
@@ -230,5 +292,7 @@ luab_module_t luab_if_nameindex_type = {
     .m_create   = if_nameindex_create,
     .m_init     = if_nameindex_init,
     .m_get      = if_nameindex_udata,
+    .m_get_tbl  = if_nameindex_checktable,
+    .m_set_tbl  = if_nameindex_pushtable,
     .m_sz       = sizeof(luab_if_nameindex_t),
 };
