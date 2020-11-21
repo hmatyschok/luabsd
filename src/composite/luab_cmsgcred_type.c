119fb1bc073274421af6621e0529b3afcfc77570
tsag <henning.matyschok@outlook.com>
2020-11-21 02:16:13 +0100

    Extension, luab_cmsgcred(3).

--- src/composite/luab_cmsgcred_type.c
+++ src/composite/luab_cmsgcred_type.c
@@ -30,6 +30,7 @@
 
 #include "luabsd.h"
 #include "luab_udata.h"
+#include "luab_table.h"
 
 #if __BSD_VISIBLE
 extern luab_module_t luab_cmsgcred_type;
@@ -326,6 +327,67 @@ cmsgcred_udata(lua_State *L, int narg)
     return (luab_to_cmsgcred(L, narg));
 }
 
+static luab_table_t *
+cmsgcred_checktable(lua_State *L, int narg)
+{
+    luab_table_t *tbl;
+    struct cmsgcred *x, *y;
+    size_t m, n, sz;
+
+    sz = sizeof(struct cmsgcred);
+
+    if ((tbl = luab_newvectornil(L, narg, sz)) != NULL) {
+
+        if (((x = (struct cmsgcred *)tbl->tbl_vec) != NULL) &&
+            (tbl->tbl_card > 1)) {
+            luab_table_init(L, 0);
+
+            for (m = 0, n = (tbl->tbl_card - 1); m < n; m++) {
+
+                if (lua_next(L, narg) != 0) {
+
+                    if ((lua_isnumber(L, -2) != 0) &&
+                        (lua_isuserdata(L, -1) != 0)) {
+                        y = luab_udata(L, -1, &luab_cmsgcred_type, struct cmsgcred *);
+                        (void)memmove(&(x[m]), y, sz);
+                    } else
+                        luab_core_err(EX_DATAERR, __func__, EINVAL);
+                } else {
+                    errno = ENOENT;
+                    break;
+                }
+                lua_pop(L, 1);
+            }
+        }
+    }
+    return (tbl);
+}
+
+static void
+cmsgcred_pushtable(lua_State *L, int narg, luab_table_t *tbl, int new, int clr)
+{
+    struct cmsgcred *x;
+    size_t m, n, k;
+
+    if (tbl != NULL) {
+
+        if (((x = (struct cmsgcred *)tbl->tbl_vec) != NULL) &&
+            ((n = (tbl->tbl_card - 1)) != 0)) {
+            luab_table_init(L, new);
+
+            for (m = 0, k = 1; m < n; m++, k++)
+                luab_rawsetudata(L, narg, &luab_cmsgcred_type, k, &(x[m]));
+
+            errno = ENOENT;
+        } else
+            errno = ERANGE;
+
+        if (clr != 0)
+            luab_table_free(tbl);
+    } else
+        errno = EINVAL;
+}
+
 luab_module_t luab_cmsgcred_type = {
     .m_cookie   = LUAB_CMSGCRED_TYPE_ID,
     .m_name     = LUAB_CMSGCRED_TYPE,
@@ -333,6 +395,8 @@ luab_module_t luab_cmsgcred_type = {
     .m_create   = cmsgcred_create,
     .m_init     = cmsgcred_init,
     .m_get      = cmsgcred_udata,
+    .m_get_tbl  = cmsgcred_checktable,
+    .m_set_tbl  = cmsgcred_pushtable,
     .m_sz       = sizeof(luab_cmsgcred_t),
 };
 #endif /* __BSD_VISBLE */
