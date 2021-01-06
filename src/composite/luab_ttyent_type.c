e870b901954573d66751bc8abc1f7031f40671ac
tsag <henning.matyschok@outlook.com>
2021-01-06 22:20:34 +0100

    Interface against ttyent{}, composite data types.

--- src/composite/luab_ttyent_type.c
+++ src/composite/luab_ttyent_type.c
@@ -62,12 +62,6 @@ typedef struct luab_ttyent {
     struct ttyent   ud_tty;
 } luab_ttyent_t;
 
-#define luab_new_ttyent(L, arg) \
-    ((luab_ttyent_t *)luab_newudata(L, &luab_ttyent_type, (arg)))
-#define luab_to_ttyent(L, narg) \
-    (luab_toldata((L), (narg), &luab_ttyent_type, \
-        struct ttyent *, luab_ttyent_type.m_sz))
-
 /*
  * Subr.
  */
@@ -116,12 +110,15 @@ ttyent_fillxtable(lua_State *L, int narg, void *arg)
 static int
 TTYENT_get_table(lua_State *L)
 {
+    luab_module_t *m;
     luab_xtable_param_t xtp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
+    m = luab_xmod(TTYENT, TYPE, __func__);
+
     xtp.xtp_fill = ttyent_fillxtable;
-    xtp.xtp_arg = luab_xdata(L, 1, &luab_ttyent_type);
+    xtp.xtp_arg = luab_xdata(L, 1, m);
     xtp.xtp_new = 1;
     xtp.xtp_k = NULL;
 
@@ -140,7 +137,9 @@ TTYENT_get_table(lua_State *L)
 static int
 TTYENT_dump(lua_State *L)
 {
-    return (luab_core_dump(L, 1, &luab_ttyent_type, luab_ttyent_type.m_sz));
+    luab_module_t *m;
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    return (luab_core_dump(L, 1, m, m->m_sz));
 }
 
 /*
@@ -159,12 +158,14 @@ TTYENT_dump(lua_State *L)
 static int
 TTYENT_ty_name(lua_State *L)
 {
+    luab_module_t *m;
     struct ttyent *typ;
     caddr_t dp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    typ = luab_udata(L, 1, &luab_ttyent_type, struct ttyent *);
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    typ = luab_udata(L, 1, m, struct ttyent *);
     dp = typ->ty_name;
 
     return (luab_pushstring(L, dp));
@@ -182,12 +183,14 @@ TTYENT_ty_name(lua_State *L)
 static int
 TTYENT_ty_getty(lua_State *L)
 {
+    luab_module_t *m;
     struct ttyent *typ;
     caddr_t dp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    typ = luab_udata(L, 1, &luab_ttyent_type, struct ttyent *);
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    typ = luab_udata(L, 1, m, struct ttyent *);
     dp = typ->ty_getty;
 
     return (luab_pushstring(L, dp));
@@ -205,12 +208,14 @@ TTYENT_ty_getty(lua_State *L)
 static int
 TTYENT_ty_type(lua_State *L)
 {
+    luab_module_t *m;
     struct ttyent *typ;
     caddr_t dp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    typ = luab_udata(L, 1, &luab_ttyent_type, struct ttyent *);
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    typ = luab_udata(L, 1, m, struct ttyent *);
     dp = typ->ty_type;
 
     return (luab_pushstring(L, dp));
@@ -228,12 +233,14 @@ TTYENT_ty_type(lua_State *L)
 static int
 TTYENT_ty_status(lua_State *L)
 {
+    luab_module_t *m;
     struct ttyent *typ;
     int x;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    typ = luab_udata(L, 1, &luab_ttyent_type, struct ttyent *);
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    typ = luab_udata(L, 1, m, struct ttyent *);
     x = typ->ty_status;
 
     return (luab_pushxinteger(L, x));
@@ -251,12 +258,14 @@ TTYENT_ty_status(lua_State *L)
 static int
 TTYENT_ty_window(lua_State *L)
 {
+    luab_module_t *m;
     struct ttyent *typ;
     caddr_t dp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    typ = luab_udata(L, 1, &luab_ttyent_type, struct ttyent *);
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    typ = luab_udata(L, 1, m, struct ttyent *);
     dp = typ->ty_window;
 
     return (luab_pushstring(L, dp));
@@ -274,12 +283,14 @@ TTYENT_ty_window(lua_State *L)
 static int
 TTYENT_ty_comment(lua_State *L)
 {
+    luab_module_t *m;
     struct ttyent *typ;
     caddr_t dp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    typ = luab_udata(L, 1, &luab_ttyent_type, struct ttyent *);
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    typ = luab_udata(L, 1, m, struct ttyent *);
     dp = typ->ty_comment;
 
     return (luab_pushstring(L, dp));
@@ -297,12 +308,14 @@ TTYENT_ty_comment(lua_State *L)
 static int
 TTYENT_ty_group(lua_State *L)
 {
+    luab_module_t *m;
     struct ttyent *typ;
     caddr_t dp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    typ = luab_udata(L, 1, &luab_ttyent_type, struct ttyent *);
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    typ = luab_udata(L, 1, m, struct ttyent *);
     dp = typ->ty_group;
 
     return (luab_pushstring(L, dp));
@@ -315,19 +328,25 @@ TTYENT_ty_group(lua_State *L)
 static int
 TTYENT_gc(lua_State *L)
 {
-    return (luab_core_gc(L, 1, &luab_ttyent_type));
+    luab_module_t *m;
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    return (luab_core_gc(L, 1, m));
 }
 
 static int
 TTYENT_len(lua_State *L)
 {
-    return (luab_core_len(L, 2, &luab_ttyent_type));
+    luab_module_t *m;
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    return (luab_core_len(L, 2, m));
 }
 
 static int
 TTYENT_tostring(lua_State *L)
 {
-    return (luab_core_tostring(L, 1, &luab_ttyent_type));
+    luab_module_t *m;
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    return (luab_core_tostring(L, 1, m));
 }
 
 /*
@@ -353,29 +372,38 @@ static luab_module_table_t ttyent_methods[] = {
 static void *
 ttyent_create(lua_State *L, void *arg)
 {
-    return (luab_new_ttyent(L, arg));
+    luab_module_t *m;
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    return (luab_newudata(L, m, arg));
 }
 
 static void
 ttyent_init(void *ud, void *arg)
 {
-    luab_udata_init(&luab_ttyent_type, ud, arg);
+    luab_module_t *m;
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    luab_udata_init(m, ud, arg);
 }
 
 static void *
 ttyent_udata(lua_State *L, int narg)
 {
-    return (luab_to_ttyent(L, narg));
+    luab_module_t *m;
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    return (luab_checkludata(L, narg, m, m->m_sz));
 }
 
 static luab_table_t *
 ttyent_checktable(lua_State *L, int narg)
 {
+    luab_module_t *m;
     luab_table_t *tbl;
     struct ttyent *x, *y;
     size_t i, j;
 
-    if ((tbl = luab_table_newvectornil(L, narg, &luab_ttyent_type)) != NULL) {
+    m = luab_xmod(TTYENT, TYPE, __func__);
+
+    if ((tbl = luab_table_newvectornil(L, narg, m)) != NULL) {
 
         if (((x = (struct ttyent *)tbl->tbl_vec) != NULL) &&
             (tbl->tbl_card > 0)) {
@@ -387,8 +415,8 @@ ttyent_checktable(lua_State *L, int narg)
 
                     if ((lua_isnumber(L, -2) != 0) &&
                         (lua_isuserdata(L, -1) != 0)) {
-                        y = luab_udata(L, -1, &luab_ttyent_type, struct ttyent *);
-                        (void)memmove(&(x[i]), y, luab_ttyent_type.m_sz);
+                        y = luab_udata(L, -1, m, struct ttyent *);
+                        (void)memmove(&(x[i]), y, m->m_sz);
                     } else
                         luab_core_err(EX_DATAERR, __func__, EINVAL);
                 } else {
@@ -405,8 +433,11 @@ ttyent_checktable(lua_State *L, int narg)
 static void
 ttyent_pushtable(lua_State *L, int narg, luab_table_t *tbl, int new, int clr)
 {
+    luab_module_t *m;
     struct ttyent *x;
     size_t i, j, k;
+
+    m = luab_xmod(TTYENT, TYPE, __func__);
 
     if (tbl != NULL) {
 
@@ -415,7 +446,7 @@ ttyent_pushtable(lua_State *L, int narg, luab_table_t *tbl, int new, int clr)
             luab_table_init(L, new);
 
             for (i = 0, j = tbl->tbl_card, k = 1; i < j; i++, k++)
-                luab_rawsetxdata(L, narg, &luab_ttyent_type, k, &(x[i]));
+                luab_rawsetxdata(L, narg, m, k, &(x[i]));
 
             errno = ENOENT;
         } else
@@ -430,7 +461,9 @@ ttyent_pushtable(lua_State *L, int narg, luab_table_t *tbl, int new, int clr)
 static luab_table_t *
 ttyent_alloctable(void *vec, size_t card)
 {
-    return (luab_table_create(&luab_ttyent_type, vec, card));
+    luab_module_t *m;
+    m = luab_xmod(TTYENT, TYPE, __func__);
+    return (luab_table_create(m, vec, card));
 }
 
 luab_module_t luab_ttyent_type = {
