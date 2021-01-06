15e1bf04ae25a730a17073525f45c02514f0998e
tsag <henning.matyschok@outlook.com>
2021-01-06 09:41:03 +0100

    Interface against regex_t, composite data types

--- src/composite/luab_regex_type.c
+++ src/composite/luab_regex_type.c
@@ -52,12 +52,6 @@ typedef struct luab_regex {
     regex_t         ud_re;
 } luab_regex_t;
 
-#define luab_new_regex(L, arg) \
-    ((luab_regex_t *)luab_newudata(L, &luab_regex_type, (arg)))
-#define luab_to_regex(L, narg) \
-    (luab_toldata((L), (narg), &luab_regex_type, \
-        regex_t *, luab_regex_type.m_sz))
-
 /*
  * Subr.
  */
@@ -104,12 +98,15 @@ regex_fillxtable(lua_State *L, int narg, void *arg)
 static int
 REGEX_get_table(lua_State *L)
 {
+    luab_module_t *m;
     luab_xtable_param_t xtp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
+    m = luab_xmod(REGEX, TYPE, __func__);
+
     xtp.xtp_fill = regex_fillxtable;
-    xtp.xtp_arg = luab_xdata(L, 1, &luab_regex_type);
+    xtp.xtp_arg = luab_xdata(L, 1, m);
     xtp.xtp_new = 1;
     xtp.xtp_k = NULL;
 
@@ -128,7 +125,9 @@ REGEX_get_table(lua_State *L)
 static int
 REGEX_dump(lua_State *L)
 {
-    return (luab_core_dump(L, 1, &luab_regex_type, luab_regex_type.m_sz));
+    luab_module_t *m;
+    m = luab_xmod(REGEX, TYPE, __func__);
+    return (luab_core_dump(L, 1, m, m->m_sz));
 }
 
 /*
@@ -147,12 +146,14 @@ REGEX_dump(lua_State *L)
 static int
 REGEX_re_magic(lua_State *L)
 {
+    luab_module_t *m;
     regex_t *re;
     int x;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    re = luab_udata(L, 1, &luab_regex_type, regex_t *);
+    m = luab_xmod(REGEX, TYPE, __func__);
+    re = luab_udata(L, 1, m, regex_t *);
     x = re->re_magic;
 
     return (luab_pushxinteger(L, x));
@@ -170,12 +171,14 @@ REGEX_re_magic(lua_State *L)
 static int
 REGEX_re_nsub(lua_State *L)
 {
+    luab_module_t *m;
     regex_t *re;
     size_t x;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    re = luab_udata(L, 1, &luab_regex_type, regex_t *);
+    m = luab_xmod(REGEX, TYPE, __func__);
+    re = luab_udata(L, 1, m, regex_t *);
     x = re->re_nsub;
 
     return (luab_pushxinteger(L, x));
@@ -193,12 +196,14 @@ REGEX_re_nsub(lua_State *L)
 static int
 REGEX_re_endp(lua_State *L)
 {
+    luab_module_t *m;
     regex_t *re;
     const char *dp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    re = luab_udata(L, 1, &luab_regex_type, regex_t *);
+    m = luab_xmod(REGEX, TYPE, __func__);
+    re = luab_udata(L, 1, m, regex_t *);
     dp = re->re_endp;
 
     return (luab_pushfstring(L, "(%p)", dp));
@@ -216,12 +221,14 @@ REGEX_re_endp(lua_State *L)
 static int
 REGEX_re_g(lua_State *L)
 {
+    luab_module_t *m;
     regex_t *re;
     caddr_t dp;
 
     (void)luab_core_checkmaxargs(L, 1);
 
-    re = luab_udata(L, 1, &luab_regex_type, regex_t *);
+    m = luab_xmod(REGEX, TYPE, __func__);
+    re = luab_udata(L, 1, m, regex_t *);
     dp = (caddr_t)(re->re_g);
 
     return (luab_pushfstring(L, "(%p)", dp));
@@ -234,19 +241,25 @@ REGEX_re_g(lua_State *L)
 static int
 REGEX_gc(lua_State *L)
 {
-    return (luab_core_gc(L, 1, &luab_regex_type));
+    luab_module_t *m;
+    m = luab_xmod(REGEX, TYPE, __func__);
+    return (luab_core_gc(L, 1, m));
 }
 
 static int
 REGEX_len(lua_State *L)
 {
-    return (luab_core_len(L, 2, &luab_regex_type));
+    luab_module_t *m;
+    m = luab_xmod(REGEX, TYPE, __func__);
+    return (luab_core_len(L, 2, m));
 }
 
 static int
 REGEX_tostring(lua_State *L)
 {
-    return (luab_core_tostring(L, 1, &luab_regex_type));
+    luab_module_t *m;
+    m = luab_xmod(REGEX, TYPE, __func__);
+    return (luab_core_tostring(L, 1, m));
 }
 
 /*
@@ -269,29 +282,38 @@ static luab_module_table_t regex_methods[] = {
 static void *
 regex_create(lua_State *L, void *arg)
 {
-    return (luab_new_regex(L, arg));
+    luab_module_t *m;
+    m = luab_xmod(REGEX, TYPE, __func__);
+    return (luab_newudata(L, m, arg));
 }
 
 static void
 regex_init(void *ud, void *arg)
 {
-    luab_udata_init(&luab_regex_type, ud, arg);
+    luab_module_t *m;
+    m = luab_xmod(REGEX, TYPE, __func__);
+    luab_udata_init(m, ud, arg);
 }
 
 static void *
 regex_udata(lua_State *L, int narg)
 {
-    return (luab_to_regex(L, narg));
+    luab_module_t *m;
+    m = luab_xmod(REGEX, TYPE, __func__);
+    return (luab_checkludata(L, narg, m, m->m_sz));
 }
 
 static luab_table_t *
 regex_checktable(lua_State *L, int narg)
 {
+    luab_module_t *m;
     luab_table_t *tbl;
     regex_t *x, *y;
     size_t i, j;
 
-    if ((tbl = luab_table_newvectornil(L, narg, &luab_regex_type)) != NULL) {
+    m = luab_xmod(REGEX, TYPE, __func__);
+
+    if ((tbl = luab_table_newvectornil(L, narg, m)) != NULL) {
 
         if (((x = (regex_t *)tbl->tbl_vec) != NULL) &&
             (tbl->tbl_card > 0)) {
@@ -303,8 +325,8 @@ regex_checktable(lua_State *L, int narg)
 
                     if ((lua_isnumber(L, -2) != 0) &&
                         (lua_isuserdata(L, -1) != 0)) {
-                        y = luab_udata(L, -1, &luab_regex_type, regex_t *);
-                        (void)memmove(&(x[i]), y, luab_regex_type.m_sz);
+                        y = luab_udata(L, -1, m, regex_t *);
+                        (void)memmove(&(x[i]), y, m->m_sz);
                     } else
                         luab_core_err(EX_DATAERR, __func__, EINVAL);
                 } else {
@@ -322,8 +344,11 @@ regex_checktable(lua_State *L, int narg)
 static void
 regex_pushtable(lua_State *L, int narg, luab_table_t *tbl, int new, int clr)
 {
+    luab_module_t *m;
     regex_t *x;
     size_t i, j, k;
+
+    m = luab_xmod(REGEX, TYPE, __func__);
 
     if (tbl != NULL) {
 
@@ -332,7 +357,7 @@ regex_pushtable(lua_State *L, int narg, luab_table_t *tbl, int new, int clr)
             luab_table_init(L, new);
 
             for (i = 0, j = tbl->tbl_card, k = 1; i < j; i++, k++)
-                luab_rawsetxdata(L, narg, &luab_regex_type, k, &(x[i]));
+                luab_rawsetxdata(L, narg, m, k, &(x[i]));
 
             errno = ENOENT;
         } else
@@ -347,7 +372,9 @@ regex_pushtable(lua_State *L, int narg, luab_table_t *tbl, int new, int clr)
 static luab_table_t *
 regex_alloctable(void *vec, size_t card)
 {
-    return (luab_table_create(&luab_regex_type, vec, card));
+    luab_module_t *m;
+    m = luab_xmod(REGEX, TYPE, __func__);
+    return (luab_table_create(m, vec, card));
 }
 
 luab_module_t luab_regex_type = {
